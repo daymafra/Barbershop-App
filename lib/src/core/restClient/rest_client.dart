855a20200cda33d83fbbc7cf94a950e53a96fc8d
Dayra <dayramafra@brq.com>
2023-08-21 00:57:27 -0300

    Ajuste de porta IP

--- lib/src/core/restClient/rest_client.dart
+++ lib/src/core/restClient/rest_client.dart
@@ -5,7 +5,7 @@ import 'package:dw_barbershop/src/core/restClient/interceptors/auth_interceptor.
 final class RestClient extends DioForNative {
   RestClient()
       : super(BaseOptions(
-          baseUrl: 'http://192.168.15.188:8080',
+          baseUrl: 'IP:PORTA',
           connectTimeout: const Duration(seconds: 30),
           receiveTimeout: const Duration(seconds: 60),
         )) {
