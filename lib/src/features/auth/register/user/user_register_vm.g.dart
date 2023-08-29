e4cf657f33f60d586cb4af357d1e0c15fd40d3a4
Dayra <dayramafra@brq.com>
2023-08-29 12:59:36 -0300

    feat: criação de tela 'Criar conta' e implementação de tela 'Cadastrar estabelecimento'

---
+++ lib/src/features/auth/register/user/user_register_vm.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_register_vm.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$userRegisterVmHash() => r'27e570454fd3d968627b22ce02c39c338bad131e';
+
+/// See also [UserRegisterVm].
+@ProviderFor(UserRegisterVm)
+final userRegisterVmProvider = AutoDisposeNotifierProvider<UserRegisterVm,
+    UserRegisterStateStatus>.internal(
+  UserRegisterVm.new,
+  name: r'userRegisterVmProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$userRegisterVmHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$UserRegisterVm = AutoDisposeNotifier<UserRegisterStateStatus>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member
