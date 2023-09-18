3d07683ae31a351535c94a381b9ad136fa700ab7
Dayra <dayramafra@brq.com>
2023-09-17 22:34:05 -0300

    Inclusão da montagem de layout, add de componente 'ScheduleCalendar', customização do HoursPanel permitindo única seleção

---
+++ lib/src/features/schedule/widgets/schedule_calendar.dart
@@ -0,0 +1,101 @@
+import 'package:dw_barbershop/src/core/ui/constants.dart';
+import 'package:dw_barbershop/src/core/ui/helpers/messages.dart';
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class ScheduleCalendar extends StatefulWidget {
+  final VoidCallback cancelPressed;
+  final ValueChanged<DateTime> okPressed;
+
+  const ScheduleCalendar({
+    super.key,
+    required this.cancelPressed,
+    required this.okPressed,
+  });
+
+  @override
+  State<ScheduleCalendar> createState() => _ScheduleCalendarState();
+}
+
+class _ScheduleCalendarState extends State<ScheduleCalendar> {
+  DateTime? selectedDay;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: const Color(0xffe6e2e9),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        children: [
+          TableCalendar(
+            availableGestures: AvailableGestures.none,
+            headerStyle: const HeaderStyle(titleCentered: true),
+            focusedDay: DateTime.now(),
+            firstDay: DateTime.utc(2010, 01, 01),
+            lastDay: DateTime.now().add(const Duration(days: 365 * 10)),
+            calendarFormat: CalendarFormat.month,
+            locale: 'PT_BR',
+            availableCalendarFormats: const {
+              CalendarFormat.month: 'Month',
+            },
+            onDaySelected: (selectedDay, focusedDay) {
+              setState(() {
+                this.selectedDay = selectedDay;
+              });
+            },
+            selectedDayPredicate: (day) {
+              return isSameDay(selectedDay, day);
+            },
+            calendarStyle: CalendarStyle(
+              selectedDecoration: const BoxDecoration(
+                color: ColorsConstants.brow,
+                shape: BoxShape.circle,
+              ),
+              todayDecoration: BoxDecoration(
+                color: ColorsConstants.brow.withOpacity(0.4),
+                shape: BoxShape.circle,
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              TextButton(
+                onPressed: widget.cancelPressed,
+                child: const Text(
+                  'Cancelar',
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
+                    color: ColorsConstants.brow,
+                  ),
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  if (selectedDay == null) {
+                    Messages.showError(
+                        'Por favor, selecione uma data', context);
+                    return;
+                  }
+                  widget.okPressed(selectedDay!);
+                },
+                child: const Text(
+                  'Ok',
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                    color: ColorsConstants.brow,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
